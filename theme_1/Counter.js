2637d41a61aaec16c6ac71e94911faa913b57623
Kimi Sian-Yu Chen <Sianyu@icloud.com>
2016-10-13 04:05:52 +0800

    add onFetchData button for test

--- theme_1/Counter.js
+++ theme_1/Counter.js
@@ -1,31 +1,35 @@
 /*eslint-disable no-unused-vars */
 import React, { Component, PropTypes } from 'react'
 
-const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>(
-      <div>
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync, onFetchData }) =>(
+    <div>
         <button onClick={onIncrement}>
-          Increment
+            Increment
         </button>
         {' '}
         <button onClick={onDecrement}>
-          Decrement
+            Decrement
         </button>
         {' '}
         <button onClick={onIncrementAsync}>
-          Increment after 1 second
+            Increment after 1 second
+        </button>
+        <button onClick={onFetchData}>
+            FetchData
         </button>
         <hr />
         <div>
-          Clicked: {value} times
+            Clicked: {value} times
         </div>
-      </div>
+    </div>
 );
 
 Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired,
-  onIncrementAsync: PropTypes.func.isRequired
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired,
+    onIncrementAsync: PropTypes.func.isRequired,
+    onFetchData : PropTypes.func.isRequired
 }
 
 export default Counter
