bd5253221d10fa5cac03750ee44cffa4b4b71b7b
Kimi Sian-Yu Chen <Sianyu@icloud.com>
2016-10-16 00:21:53 +0800

    add counter2 for test

--- theme_1/Counter.js
+++ theme_1/Counter.js
@@ -1,6 +1,59 @@
 /*eslint-disable no-unused-vars */
 import React, { Component, PropTypes } from 'react'
 
+
+ class Counter2 extends Component {
+
+     constructor(props) {
+         super(props);
+
+         this.state = {
+             maxLength: this.props.maxLength,
+         };
+     }
+
+     static defaultProps = {
+         checked: false,
+         maxLength: 10,
+     };
+
+     static propTypes = {
+         checked: React.PropTypes.bool.isRequired,
+         maxLength: React.PropTypes.number.isRequired
+     };
+
+    render() {
+
+        const { value, onIncrement, onDecrement, onIncrementAsync, onFetchData  } = this.props;
+
+        return (
+
+            <div>
+                <button onClick={onIncrement}>
+                    Increment1
+                </button>
+                {' '}
+                <button onClick={onDecrement}>
+                    Decrement
+                </button>
+                {' '}
+                <button onClick={onIncrementAsync}>
+                    Increment after 1 second
+                </button>
+                <button onClick={onFetchData}>
+                    FetchData
+                </button>
+                <hr />
+                <div>
+                    Clicked: {value} times
+                </div>
+            </div>
+        )
+    }
+}
+
+/*
+
 const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync, onFetchData }) =>(
     <div>
         <button onClick={onIncrement}>
@@ -32,4 +85,6 @@ Counter.propTypes = {
     onFetchData : PropTypes.func.isRequired
 }
 
-export default Counter
+*/
+
+export default Counter2
