c441f5404c842132f4b77f638fae373c61e2a981
Kimi Sian-Yu Chen <Sianyu@icloud.com>
2016-11-07 22:58:03 +0800

    Add store.subscribe

--- theme_1/main.js
+++ theme_1/main.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
-/*
+
 import Counter from './Counter'
 import reducer from './reducers'
 import rootSaga from './sagas'
@@ -22,7 +22,7 @@ sagaMiddleware.run(helloSaga);
 
 const action = type => store.dispatch({type})
 
-*/
+
 
 import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
@@ -31,11 +31,13 @@ function render() {
   ReactDOM.render(
 
       //<App />,
+      /*
       <MyAwesomeReactComponent
 
       />,
+      */
 
-      /*
+
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
@@ -43,7 +45,7 @@ function render() {
       onIncrementAsync={() => action('INCREMENT_ASYNC')}
       onFetchData={() => action('FETCHDATA')}
     />,
-    */
+
 
 
     document.getElementById('root')
@@ -51,4 +53,69 @@ function render() {
 }
 
 render();
-//store.subscribe(render);
+
+//增加store.subscribe
+//Add store.subscribe
+store.subscribe(render);
+
+
+/*
+ import "babel-polyfill"
+
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ import { createStore, applyMiddleware } from 'redux'
+ import createSagaMiddleware from 'redux-saga'
+
+ import Counter from './Counter'
+ import reducer from './reducers'
+ import rootSaga from './sagas'
+
+ const sagaMiddleware = createSagaMiddleware()
+ const store = createStore(
+ reducer,
+ applyMiddleware(sagaMiddleware)
+ )
+ sagaMiddleware.run(rootSaga)
+
+ import { helloSaga } from './sagas'
+ sagaMiddleware.run(helloSaga);
+
+ const action = type => store.dispatch({type})
+
+
+ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+ import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
+ const App = () => (
+ <MuiThemeProvider>
+ <MyAwesomeReactComponent />
+ </MuiThemeProvider>
+ );
+
+
+
+
+
+ function render() {
+ ReactDOM.render(
+
+ //<App />,
+
+ <Counter
+ value={store.getState()}
+ onIncrement={() => action('INCREMENT')}
+ onDecrement={() => action('DECREMENT')}
+ onIncrementAsync={() => action('INCREMENT_ASYNC')}
+ onFetchData={() => action('FETCHDATA')}
+
+ />,
+
+ document.getElementById('root')
+ )
+ }
+
+ render()
+ store.subscribe(render)
+
+ */
